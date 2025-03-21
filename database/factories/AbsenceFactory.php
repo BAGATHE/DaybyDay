baf136606ee8eeb51917185bc987f968c36f7dfb
BAGATHE <lolemadaly@gmail.com>
2025-03-21 14:11:04 +0300

    initialisation donné teste

--- database/factories/AbsenceFactory.php
+++ database/factories/AbsenceFactory.php
@@ -2,14 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+
 use App\Models\Absence;
 use App\Models\User;
 use Faker\Generator as Faker;
+use App\Enums\AbsenceReason;
 
 $factory->define(Absence::class, function (Faker $faker) {
     return [
         'external_id' => \Ramsey\Uuid\Uuid::uuid4()->toString(),
-        'reason' => $faker->word,
+        'reason' => array_rand(AbsenceReason::values()),
         'start_at' => now(),
         'end_at' => now()->addDays(3),
         'user_id' => factory(User::class),
